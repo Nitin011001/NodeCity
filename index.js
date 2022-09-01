c60bae61c8fbcb8cab812f9c5f641a9cca4e0b4c
Nitin011001 <thenitinchauhan01@gmail.com>
2022-08-31 21:06:29 -0400

    added packages

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
-const replaceTemplate=require('./modules/replaceTemplate');
 
+const slugify = require("slugify");
+
+const replaceTemplate = require("./modules/replaceTemplate");
 
 const tempOverView = fs.readFileSync(
   `${__dirname}/templetes/template-overview.html`,
@@ -12,15 +14,15 @@ const tempCard = fs.readFileSync(
   `${__dirname}/templetes/template-card.html`,
   "utf-8"
 );
-console.log(tempCard)
+
 const tempPlace = fs.readFileSync(
   `${__dirname}/templetes/template-place.html`,
   "utf-8"
 );
 const jsonData = fs.readFileSync(`${__dirname}/data/data.json`, "utf-8");
 const jsonObject = JSON.parse(jsonData);
-console.log(replaceTemplate(tempCard,jsonObject[1]))
-
+const slugs=jsonObject.map(el=>slugify(el.placeName,{lower:true}));
+console.log(slugs);
 // console.log(jsonObject);
 
 const server = http.createServer((req, res) => {
@@ -28,11 +30,11 @@ const server = http.createServer((req, res) => {
   //Overview route
   if (pathname === "/" || pathname === "/overview") {
     res.writeHead(200, { "Content-type": "text/html" });
-    console.log(query)
+    console.log(query);
     const cardHtml = jsonObject
       .map((el) => replaceTemplate(tempCard, el))
       .join("");
-      console.log(cardHtml)
+
     const output = tempOverView.replace("{%PLACE-CARDS%}", cardHtml);
     // console.log(output);
     res.end(output);
